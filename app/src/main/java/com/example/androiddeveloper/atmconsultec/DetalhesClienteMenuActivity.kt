3a34326bed4f6e7a2f5eb89590428b02c65f3632
Renato Dancini <renato.dancini@gmail.com>
2019-01-02 17:25:30 -0200

    ""

---
+++ app/src/main/java/com/example/androiddeveloper/atmconsultec/DetalhesClienteMenuActivity.kt
@@ -0,0 +1,12 @@
+package com.example.androiddeveloper.atmconsultec
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class DetalhesClienteMenuActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detalhes_cliente_menu)
+    }
+}
