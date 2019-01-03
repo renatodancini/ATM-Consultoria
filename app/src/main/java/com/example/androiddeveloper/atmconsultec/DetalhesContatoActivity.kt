0bfeb4aa43f50bcdf7f064c59f3a4406fb53377f
Renato Dancini <renato.dancini@gmail.com>
2019-01-03 16:58:58 -0200

    Fluxo de telas finalizados
    Signed-off-by: Renato Dancini <renato.dancini@gmail.com>

---
+++ app/src/main/java/com/example/androiddeveloper/atmconsultec/DetalhesContatoActivity.kt
@@ -0,0 +1,12 @@
+package com.example.androiddeveloper.atmconsultec
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class DetalhesContatoActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detalhes_contato)
+    }
+}
